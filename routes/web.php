3655e182419edc127b80e894e90ea0a60625c659
abdelhaymallouli <abdelhay.mallouli@gmail.com>
2025-11-12 08:13:15 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/ping', fn() => 'pong');
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/a-propos', [PageController::class, 'about'])->name('about');
 Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
